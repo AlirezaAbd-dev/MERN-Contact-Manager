2396f6f15e8b0f2335c76b1185ef34ecd9294a6d
Shervin <68682979+shervinbdndev@users.noreply.github.com>
2022-10-08 02:06:36 +0330

    Delete server directory

--- server/src/app/middlewares/Error.ts
+++
@@ -1,8 +0,0 @@
-import {ErrorRequestHandler} from "express";
-import {logger} from "../index";
-
-export const ErrorMiddleware:ErrorRequestHandler=(err,req, res,next)=>{
-    console.log(err.stack)
-    logger.error(err.stack)
-    next()
-}
--- server/src/app/models/groups.ts
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-
-const schema = new mongoose.Schema({
-    groups:Array
-})
-
-export const groupModel = mongoose.model('groups', schema);
